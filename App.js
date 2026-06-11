d2bf36c0c26486c3e0aba3ae839ab957ec54eed8
MIGUEL CARDOSO <miguel.cardoso14@etec.sp.gov.br>
2026-06-11 10:00:41 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AboutScreen from './src/screens/AboutScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ah n sei oq n sei oq la</Text>
       <StatusBar style="auto" />
     </View>
   );
